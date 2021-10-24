c9b3bd6f96f1197846861f30e17b1b302ce3b991
Adjetey Vincent <adjeteyspiritus1@gmail.com>
2021-10-24 00:23:31 +0000

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionComponent } from './question/question.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    QuestionComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
