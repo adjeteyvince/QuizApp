ce1501132ab689c6caef2613f86c99b5b9fd1bb4
Adjetey Vincent <adjeteyspiritus1@gmail.com>
2021-10-26 18:24:23 +0000

    design header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'MyQuizApp';
+
+  state: String = "Start";
+
+  switchState(){
+    switch (this.state) {
+      case "Start": this.state = "Submit"; break;
+      case "Submit": this.state = "Retry"; break;
+      case "Retry": this.state = "Start"; break;
+    }
+  }
 }
